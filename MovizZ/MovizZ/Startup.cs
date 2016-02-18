f7773e7d37fee1603e65b48c1f3f5e0ea61c3800
Ali Mohammed <ali.mohammed.dev@outlook.fr>
2016-02-18 12:58:08 +0100

    Initial commit

---
+++ MovizZ/MovizZ/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovizZ.Startup))]
+namespace MovizZ
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
